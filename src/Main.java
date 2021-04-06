ff3f678952fd75ef2387c1ae1d710460e981ea51
fradvocatus <alfriyadi.rafles@binus.ac.id>
2021-04-06 11:08:31 +0700

    Completed variables

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
+        // Variables
 
+        // integer
+        // can contain only whole numbers.
+        int myVariable = 12; // variable declaration, can only be done once.
+        myVariable = 14;
+
+        // String
+        // can contain words, sentences and paragraphs.
+        String myString = "Fred";
+
+        // float
+        // can contain whole numbers or decimal separated values
+        double myFloat = 24.5;
+
+        // char
+        // can only contain one letter
+        char myChar = 'a';
+
+        // boolean
+        // can only contain true or false
+        boolean myBoolean = true;
+
+        //printing out to console
+        System.out.println(myVariable + 1);
     }
 }
